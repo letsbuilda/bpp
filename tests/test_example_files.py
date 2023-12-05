dbf389dbcf6aedc236457472ebb3c7ce3a3b8533
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-12-04 22:54:36 -0600

    Add more example files
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

---
+++ tests/test_example_files.py
@@ -0,0 +1,29 @@
+"""Can execute example files and return expected results."""
+
+from io import StringIO
+from pathlib import Path
+
+import pytest
+from bpp import Interpreter
+
+
+@pytest.mark.parametrize(
+    ("example_file", "expected_result"),
+    [
+        (Path("./examples/hello-world.bf"), "Hello, World!"),
+        (Path("./examples/jump.bf"), ""),
+    ],
+)
+def test_example_files(example_file: Path, expected_result: str) -> None:
+    """Test example files."""
+    source = example_file.read_text()
+    interpreter = Interpreter()
+    assert interpreter.run(source) == expected_result
+
+
+def test_example_files_with_input(monkeypatch) -> None:  # noqa: ANN001 -- pytest builtin fixture
+    """Test example files with input."""
+    source = Path("./examples/echo.bf").read_text()
+    monkeypatch.setattr("sys.stdin", StringIO("a"))
+    interpreter = Interpreter()
+    assert interpreter.run(source) == "a"
