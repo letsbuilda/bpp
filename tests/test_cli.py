8670e2a67a029bced0c5b3a62d945b3d98f8907f
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-12-04 22:26:01 -0600

    Add test for __main__.py
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,7 @@
 """Test the CLI."""
 
+from runpy import run_module
+
 from bpp.cli import app
 from typer.testing import CliRunner
 
@@ -11,3 +13,9 @@ def test_app() -> None:
     result = runner.invoke(app, ["run", r"./examples/hello-world.bf"])
     assert result.exit_code == 0
     assert "Hello, World!" in result.stdout
+
+
+def test_dash_m() -> None:
+    """`python -m bpp` works."""
+    output_globals = run_module("bpp")
+    assert all(global_.startswith("__") or global_ == "app" for global_ in output_globals)
