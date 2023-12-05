5c50db247f5f25d4fbc484e8f24639f7de37b65f
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-12-04 22:09:21 -0600

    Try slashes the other way around
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,6 +8,6 @@ runner = CliRunner()
 
 def test_app() -> None:
     """Initial test for the CLI."""
-    result = runner.invoke(app, ["run", r".\examples\hello-world.bf"])
+    result = runner.invoke(app, ["run", r"./examples/hello-world.bf"])
     assert result.exit_code == 0
     assert "Hello, World!" in result.stdout
